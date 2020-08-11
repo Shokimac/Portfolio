d20ef45f2cd9de78a886653939042ece97cf4a71
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-08-11 11:52:16 +0900

    [Add]_episode_new_create

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,8 +1,14 @@
 class EpisodesController < ApplicationController
   def new
+    @episode = Episode.new
   end
 
   def create
+    @episode = Episode.new(episode_params)
+    @episode.user_id = current_user.id
+    binding.pry
+    @episode.save
+    redirect_to episode_path(@episode)
 
   end
 
@@ -21,6 +27,12 @@ class EpisodesController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def episode_params
+    params.require(:episode).permit(:title, :body)
   end
 
 end
