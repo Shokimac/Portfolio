41e2d87b96b1ca92cabb5428d943daadb279a513
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-08-11 14:21:25 +0900

    [Add]_destroy_comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,13 +3,15 @@ class PostCommentsController < ApplicationController
         @proverb = Proverb.find(params[:proverb_id])
         comment = current_user.post_comments.new(comment_params)
         comment.proverb_id = @proverb.id
-        binding.pry
         comment.save
         redirect_to proverb_path(@proverb)
     end
 
     def destroy
-        
+        comment = PostComment.find(params[:id])
+        binding.pry
+        comment.destroy
+        redirect_to proverb_path(params[:proverb_id])
     end
 
     private
