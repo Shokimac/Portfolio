e0f7779b8f886edbab055e97dc7a490631d783a0
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-08-13 16:26:59 +0900

    [Add]_episode_favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,6 +7,13 @@ class FavoritesController < ApplicationController
             favorite.proverb_id = @proverb.id
             favorite.save
             redirect_to proverb_path(@proverb)
+            
+        when "1"
+            @episode = Episode.find(params[:episode_id])
+            favorite = current_user.episode_favorites.new
+            favorite.episode_id = @episode.id
+            favorite.save
+            redirect_to episode_path(@episode)
         end
 
     end
@@ -18,6 +25,12 @@ class FavoritesController < ApplicationController
             favorite = ProverbFavorite.find_by(proverb_id: @proverb.id, user_id: current_user.id)
             favorite.destroy
             redirect_to proverb_path(@proverb)
+        when "1"
+            @episode = Episode.find(params[:episode_id])
+            favorite = EpisodeFavorite.find_by(episode_id: @episode.id, user_id: current_user.id)
+            binding.pry
+            favorite.destroy
+            redirect_to episode_path(@episode)
         end
     end
 
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -4,4 +4,7 @@ class Episode < ApplicationRecord
     has_many :episode_favorites
     has_many :episode_bookmarks
     
+    def favorited_by?(user)
+        episode_favorites.where(user_id: user.id).exists?
+    end
 end
