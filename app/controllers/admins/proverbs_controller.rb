d2e3eb9cb6d69751083675f34ac3009a998d2d61
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-08-19 15:33:54 +0900

    [Fix]_ProverbShow_view

--- app/controllers/admins/proverbs_controller.rb
+++ app/controllers/admins/proverbs_controller.rb
@@ -1,4 +1,6 @@
 class Admins::ProverbsController < ApplicationController
   def show
+    @proverb = Proverb.find(params[:id])
+    @post_comments = PostComment.where(proverb_id: @proverb.id)
   end
 end
