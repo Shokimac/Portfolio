80f4ba065e661d54d9e5ae9e302ddbf5dcef8773
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-08-20 23:59:37 +0900

    [Fix]_ProverbBookmark

--- app/controllers/proverb_bookmarks_controller.rb
+++ app/controllers/proverb_bookmarks_controller.rb
@@ -4,13 +4,11 @@ class ProverbBookmarksController < ApplicationController
         bookmark = current_user.proverb_bookmarks.new
         bookmark.proverb_id = @proverb.id
         bookmark.save
-        redirect_to proverb_path(@proverb)
     end
 
     def destroy
         @proverb = Proverb.find(params[:proverb_id])
         bookmark = ProverbBookmark.find_by(proverb_id: @proverb.id, user_id: current_user.id)
         bookmark.destroy
-        redirect_to proverb_path(@proverb)
     end
 end
