c3f6f290258517e9cdeccf0924cb262a7c692cbd
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-08-22 23:58:10 +0900

    [Fix]_TopView_design

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   def index
-
+    @episodes = EpisodeBookmark.where(user_id: current_user.id).page(params[:page]).reverse_order
+    @proverbs = ProverbBookmark.where(user_id: current_user.id).page(params[:page]).reverse_order
   end
 end
