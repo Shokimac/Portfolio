91e06648ee62882343f508fba075f5533dd27d5d
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-08-24 22:41:26 +0900

    SendTest_OK

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,9 +2,9 @@ class InquiryMailer < ApplicationMailer
     def send_mail(inquiry)
         @inquiry = inquiry
         mail(
-          from: 'system@example.com',
-          to:   'manager@example.com',
-          subject: 'お問い合わせ通知'
+          from: 'curin020304@gmail.com',
+          to:   'curin030204@yahoo.co.jp',
+          subject: 'お問い合わせがありました'
         )
       end
 end
