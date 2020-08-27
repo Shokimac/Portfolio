c516f4295b0cebfd207f059a9dc93dbe534e270b
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-08-27 17:31:34 +0900

    [Add]_Signup_with_Name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[:name])
+    added_attrs = [ :email, :name, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
   end
-
 end
