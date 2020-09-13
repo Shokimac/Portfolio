3818a9eac9ff57ccf7352396b410a14c489f1293
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-13 16:09:07 +0900

    [Add]_SleepToUserControllerShow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @episodes = Episode.includes(:user).where(user_id: @user.id).page(params[:page]).reverse_order
     @proverbs = Proverb.includes(:user).where(user_id: @user.id).page(params[:page]).reverse_order
+    sleep(3)
     @image_url = "https://image-resize-itowokashi.s3-ap-northeast-1.amazonaws.com/store/" + @user.image_id + "-thumbnail."
   end
 
