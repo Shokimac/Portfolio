dc19395ede2007ae0b50ccec1b8b2bdedd852d69
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-16 16:06:03 +0900

    Delete_test_directory

--- test/controllers/inquiry_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class InquiryControllerTest < ActionDispatch::IntegrationTest
-  test "should get inquiry" do
-    get inquiry_inquiry_url
-    assert_response :success
-  end
-
-end
--- test/models/proverb_favorite_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProverbFavoriteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
