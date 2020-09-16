fa5ab7e5c882147861f79e60770be945e23d5b89
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-16 10:41:54 +0900

    [Add]_UserModel_Association_Test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Userモデルのテスト', type: :model do
+  describe 'アソシエーションのテスト' do
+    context 'Episodeモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:episodes).macro).to eq :has_many  
+      end
+    end
+    
+    context 'EpisodeFavoriteモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:episode_favorites).macro).to eq :has_many 
+      end
+    end
+
+    context 'EpisodeBookmarkモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:episode_bookmarks).macro).to eq :has_many 
+      end
+    end
+
+    context 'Proverbモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:proverbs).macro).to eq :has_many 
+      end
+    end
+
+    context 'ProverbFavoriteモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:proverb_favorites).macro).to eq :has_many 
+      end
+    end
+
+    context 'ProverbBookmarkモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:proverb_bookmarks).macro).to eq :has_many 
+      end
+    end
+
+    context 'PostCommentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:post_comments).macro).to eq :has_many 
+      end
+    end    
+  end
+
 end
