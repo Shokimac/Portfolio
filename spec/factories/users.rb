f67d93a2a8c842aa0e835e20241c2844fbc798c7
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-16 16:05:24 +0900

    [Add]_test_user2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     password { 'password' }
     password_confirmation { 'password' }
   end
+
+  factory :user2, class:User do
+    name { Faker::Lorem.characters(number:10) }
+    sequence(:email) { |n| "testuser#{n}@sample.com" }
+    introduction { Faker::Lorem.characters(number:20) }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
 end
