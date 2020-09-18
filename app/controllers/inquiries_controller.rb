a45ce077f6ab8499d321cc2f21d9be1dee0d1cf5
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-18 16:18:29 +0900

    [Fix]_inquiries_controller_create_action

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -9,7 +9,7 @@ class InquiriesController < ApplicationController
     InquiryMailer.send_mail(@inquiry).deliver_now
     redirect_to episodes_path
     else
-      @render.errors.full_messages
+      @inquiry.errors.full_messages
       render :new
     end
   end
