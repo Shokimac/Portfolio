62e1665464052730605238ba1228ed9a2970e954
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-18 22:20:34 +0900

    [Add]_bookmark_index_display_test

--- spec/system/bookmark_spec.rb
+++ spec/system/bookmark_spec.rb
@@ -1,16 +1,33 @@
 require 'rails_helper'
 
 describe "ブックマーク" do
-    let(:user) { create(:user) }
-    let(:user2) { create(:user) }
-    let(:user2_episode) { create(:episode, user: user2) }
-    let(:user2_proverb) { create(:proverb, user: user2) }
+    let!(:user) { create(:user) }
+    let!(:user2) { create(:user) }
+    let!(:user2_episode) { create(:episode, user: user2) }
+    let!(:user2_proverb) { create(:proverb, user: user2) }
 
     before do
         visit new_user_session_path
         fill_in 'user[email]', with: user.email
         fill_in 'user[password]', with: user.password
         click_button 'ログイン'
+    end
+
+    context "お気に入り一覧画面表示確認" do
+        before do
+            visit episode_path(user2_episode)
+            click_link 
+
+            visit user_bookmarks_path(user)
+        end
+
+        it '「ユーザー名 さんのお気に入り」と表示されている' do
+            expect(page).to have_content("#{user.name} さんのお気に入り") 
+        end
+
+        it '「お気に入りのエピソード」と表示されている' do
+            expect(page).to have_content('お気に入りのエピソード')
+        end
     end
     
 end
