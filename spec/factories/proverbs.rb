f4980f8fa97dd0fdcf92d7b2770da89b3a12493e
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-18 12:38:22 +0900

    [Fix]_spell_miss

--- spec/factories/proverbs.rb
+++ spec/factories/proverbs.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
         user
         name { Faker::Lorem.characters(number:10) }
         body { Faker::Lorem.characters(number:25) }
-        inteoduction { Faker::Lorem.characters(number:50) }
+        introduction { Faker::Lorem.characters(number:50) }
     end
 end
