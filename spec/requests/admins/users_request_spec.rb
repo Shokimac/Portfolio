14b021015423d2073e654e87c571aad61773fb26
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-19 16:50:34 +0900

    [Add]_admins_users_request

--- spec/requests/admins/users_request_spec.rb
+++ spec/requests/admins/users_request_spec.rb
@@ -1,22 +1,27 @@
 require 'rails_helper'
 
-describe "管理者 ユーザー関連 アクセステスト" do
-
-    describe "非ログイン時" do
-
-        context '以下のアクションにアクセスできない' do
+    describe "管理者[ユーザー関連] 非ログイン時" do
+    let!(:user) { create(:user) }
+    
+    context '以下のアクションにアクセスできない' do
+        it 'ユーザー 一覧ページ' do
+            get admins_users_path
+                expect(response).to have_http_status 302
+            end
             
-        end
-
-    end
-
-    describe "ユーザー(非管理者)ログイン時" do
-        
-        context '以下のアクションにアクセスできない' do
+            it 'ユーザー詳細ページ' do
+                get admins_user_path(user)
+                expect(response).to have_http_status 302
+            end
             
+            it 'ユーザー退会アクション' do
+                patch admins_user_withdrawal_path(user)
+                expect(response).to have_http_status 302
+            end
+            
+            it 'ユーザー退会復帰アクション' do
+                patch admins_user_return_path(user)
+                expect(response).to have_http_status 302
+            end
         end
     end
-    
-    
-    
-end
