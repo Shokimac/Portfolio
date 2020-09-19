fa1fa075f5d82818e4dd4dda71d08961a0c34c30
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-19 16:18:13 +0900

    Make_admins_request_files

---
+++ spec/requests/admins/users_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "管理者 ユーザー関連 アクセステスト" do
+
+    describe "非ログイン時" do
+
+        context '以下のアクションにアクセスできない' do
+            
+        end
+
+    end
+
+    describe "ユーザー(非管理者)ログイン時" do
+        
+        context '以下のアクションにアクセスできない' do
+            
+        end
+    end
+    
+    
+    
+end
