c0c2b072f45b618757a56cd200783fb2528be9ac
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-19 15:45:52 +0900

    [Add]_episode_request_test

--- spec/requests/episode_request_spec.rb
+++ spec/requests/episode_request_spec.rb
@@ -5,6 +5,40 @@ describe "[エピソード関連]非ログイン時のアクセステスト" do
     let(:episode) { create(:episode, user: user) }
 
     context "以下のアクションにはアクセスできない" do
+        
+        it 'エピソード投稿ページ' do
+            get new_episode_path
+            expect(response).to have_http_status 302
+        end
 
+        it 'エピソード新規投稿アクション' do
+            post episodes_path
+            expect(response).to have_http_status 302
+        end
 
+        it 'エピソード詳細ページ' do
+            get episode_path(episode)
+            expect(response).to have_http_status 302
+        end
+
+        it 'エピソードタイムラインページ' do
+            get episodes_path
+            expect(response).to have_http_status 302
+        end
+
+        it 'エピソード編集ページ' do
+            get edit_episode_path(episode)
+            expect(response).to have_http_status 302
+        end
+
+        it 'エピソード更新アクション' do
+            patch episode_path(episode)
+            expect(response).to have_http_status 302
+        end
+
+        it 'エピソード削除アクション' do
+            delete episode_path(episode)
+            expect(response).to have_http_status 302
+        end
+    end
 end
