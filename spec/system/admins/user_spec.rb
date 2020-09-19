09bbfc72f0aeea55c6aef79f2386ef1a919e7027
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-19 18:30:11 +0900

    [Add]_admins_user_index_display_test

--- spec/system/admins/user_spec.rb
+++ spec/system/admins/user_spec.rb
@@ -21,4 +21,80 @@ describe "管理者認証" do
             end
         end
     end
+
+    describe "管理者作成" do
+        context "管理者新規登録画面" do
+            it '遷移できない' do
+                expect {
+                    visit '/admins/sign_up'
+                }.to raise_error('No route matches [GET] "/admins/sign_up"')
+            end
+        end
+    end
+end
+
+describe "管理者 ユーザー関連のテスト" do
+    let!(:user) { create(:user) }
+    let!(:user2) { create(:user) }
+    let!(:user4) { create(:user) }
+    let!(:episode) { create(:episode, user: user) }
+    let!(:proverb) { create(:proverb, user: user2) }
+
+    before do
+        visit new_admin_session_path
+        fill_in "admin[email]",	with: ENV['ADMIN_EMAIL']
+        fill_in "admin[password]",	with: ENV['ADMIN_PASSWORD']
+        click_button 'ログイン'
+    end
+
+    context "ユーザー 一覧の表示確認" do
+        before do
+            visit admins_users_path
+        end
+
+        it 'ラベルに「ユーザー名」と表示されている' do
+            expect(page).to have_content('ユーザー名') 
+        end
+
+        it 'ラベルに「メールアドレス」と表示されている' do
+            expect(page).to have_content('メールアドレス') 
+        end
+
+        it 'ラベルに「投稿エピソード件数」と表示されている' do
+            expect(page).to have_content('投稿エピソード件数') 
+        end
+
+        it 'ラベルに「投稿格言数」と表示されている' do
+            expect(page).to have_content('投稿格言数') 
+        end
+
+        it 'ラベルに「退会済フラグ」と表示されている' do
+            expect(page).to have_content('退会済フラグ') 
+        end
+
+        it 'ユーザーのプロフィール画像が表示されている' do
+            expect(page).to have_css '.userIndex' 
+        end
+
+        it 'ユーザー名が表示されている' do
+            expect(page).to have_content user.name 
+        end
+
+        it 'メールアドレスが表示されている' do
+            expect(page).to have_content user.email 
+        end
+
+        it '投稿エピソード件数が表示されている' do
+            expect(page).to have_content user.episodes.count
+        end
+
+        it '投稿格言数が表示されている' do
+            expect(page).to have_content user.proverbs.count
+        end
+
+        it '退会フラグが表示されている' do
+            expect(page).to have_css('.userIndex__body--check')
+        end
+    end
+
 end
