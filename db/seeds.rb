a0da06f4e047be9c70d0ca69fa6a47ad73a6048c
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-19 17:32:27 +0900

    [Fix]_db_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(email:'admin@sample.com',password:'itoito')
+Admin.create!(email:ENV['ADMIN_EMAIL'],password:ENV['ADMIN_PASSWORD'])
 User.create!(name:'テストユーザー１',email:'ito@sample.com',password:'itoito')
 DroppedLetter.create!(body:"テスト")
