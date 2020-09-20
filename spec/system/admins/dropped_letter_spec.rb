40eb8f1517ff6610086a4518305411e9ca169f38
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-20 15:12:02 +0900

    [Add]_admins_letter_edit_display_test

--- spec/system/admins/dropped_letter_spec.rb
+++ spec/system/admins/dropped_letter_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 describe "管理者 落とし文関連のテスト" do
-
     let!(:letter) { create(:dropped_letter) }
-    let!(:letter2) { create(:dropped_letter) }
     before do
         visit new_admin_session_path
         fill_in "admin[email]",	with: ENV['ADMIN_EMAIL']
@@ -11,7 +9,9 @@ describe "管理者 落とし文関連のテスト" do
         click_button 'ログイン'
     end
 
-    context "落とし文一覧画面の表示確認" do
+    describe "落とし文一覧画面" do
+        
+        context "表示確認" do
 
         before do
             visit admins_dropped_letters_path
@@ -35,7 +35,6 @@ describe "管理者 落とし文関連のテスト" do
 
         it '投稿した落とし文が表示されている' do
             expect(page).to have_content letter.body
-            expect(page).to have_content letter2.body
         end
 
         it '非表示フラグが立っている落とし文には非表示と出る' do
@@ -47,6 +46,16 @@ describe "管理者 落とし文関連のテスト" do
         end
     end
 
+    context "リンク先の確認" do
+        before do
+            visit admins_dropped_letters_path
+        end
+
+        it '編集ボタンのリンク先が正しい' do
+            click_link 'letter0'
+            expect(current_path).to eq edit_admins_dropped_letter_path(letter) 
+        end
+    end
 
     context "新規投稿テスト" do
         before do
@@ -65,6 +74,44 @@ describe "管理者 落とし文関連のテスト" do
             expect(page).to have_content("can't be blank") 
         end
     end
-    
+end
+end
+
+describe "落とし文編集画面" do
+    let(:letter) { create(:dropped_letter) }
+    before do
+        visit new_admin_session_path
+        fill_in "admin[email]",	with: ENV['ADMIN_EMAIL']
+        fill_in "admin[password]",	with: ENV['ADMIN_PASSWORD']
+        click_button 'ログイン'
+        visit edit_admins_dropped_letter_path(letter)
+    end
+
+    context "表示確認" do
+        
+        it '「落とし文編集」と表示されている' do
+            expect(page).to have_content('落とし文編集') 
+        end
+
+        it '落とし文入力フォームが表示されている' do
+            expect(page).to have_field 'dropped_letter[body]', with: letter.body
+        end
+
+        it 'ラベルに「表示ステータス」と表示されている' do
+            expect(page).to have_content('表示ステータス') 
+        end
+
+        it 'ラジオボタンの表示側がデフォルトでチェック' do
+            expect(page).to have_checked_field '表示'
+        end
+
+        it 'ラジオボタンの非表示側はチェックされていない' do
+            expect(page).to_not have_checked_field '非表示'
+        end
+
+        it '更新ボタンが表示されている' do
+            expect(page).to have_button '更新' 
+        end
+    end
     
 end
