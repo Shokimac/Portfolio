ba8ac02365491aafe50ec9baa814b9d876b56f43
横溝 翔貴 <curin030204@yahoo.co.jp>
2020-09-22 22:38:55 +0900

    [Add]_proverb_book&favo_if_action_failure

--- app/controllers/proverb_bookmarks_controller.rb
+++ app/controllers/proverb_bookmarks_controller.rb
@@ -6,12 +6,24 @@ class ProverbBookmarksController < ApplicationController
         @proverb = Proverb.find(params[:proverb_id])
         bookmark = current_user.proverb_bookmarks.new
         bookmark.proverb_id = @proverb.id
-        bookmark.save
+        unless bookmark.save
+            @proverb = Proverb.find(params[:id])
+            @user = User.find(@proverb.user_id)
+            @comment = PostComment.new
+            @comments = PostComment.where(proverb_id: @proverb.id).includes(:user, :proverb).page(params[:page]).reverse_order
+            render 'proverbs/show'
+        end
     end
 
     def destroy
         @proverb = Proverb.find(params[:proverb_id])
         bookmark = ProverbBookmark.find_by(proverb_id: @proverb.id, user_id: current_user.id)
-        bookmark.destroy
+        unless bookmark.destroy
+            @proverb = Proverb.find(params[:id])
+            @user = User.find(@proverb.user_id)
+            @comment = PostComment.new
+            @comments = PostComment.where(proverb_id: @proverb.id).includes(:user, :proverb).page(params[:page]).reverse_order
+            render 'proverbs/show'
+        end
     end
 end
